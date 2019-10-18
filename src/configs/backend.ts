8a49f82bf49039b983b8811600bb387ab19c08fe
Samuel Rousseau <samuelrb@dotmedias.com>
2019-10-17 22:34:40 -0400

    Adding prod URLs

--- src/configs/backend.ts
+++ src/configs/backend.ts
@@ -1,4 +1,9 @@
-export const backend = {
-  endPointClean: "http://localhost:55935/api/v1/clean",
-  endPointNotify: "http://localhost:55935/api/v1/notify"
+export const backendLocal = {
+  endPointClean: "http://localhost:51461/api/v1/clean",
+  endPointNotify: "http://localhost:51461/api/v1/notify"
 }
+export const backendProd = {
+  endPointClean: "https://api.pasteitcleaned.io/v1/clean",
+  endPointNotify: "https://api.pasteitcleaned.io/v1/notify"
+}
+export const backend = backendProd
